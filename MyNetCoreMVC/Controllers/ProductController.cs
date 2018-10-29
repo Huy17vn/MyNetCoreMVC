89eb3545635ae5ef7ddf370a90f5c3c49f54f96d
Huy Phạm <binhaibin123@gmail.com>
2018-10-29 17:22:18 +0700

    bai tap

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreMVC.Models;
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // 
+        //[Route("Huy/Index/{id:int}")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetList()
+        {
+            return View();
+        }
+
+        public IActionResult Create( string name, string price)
+        {
+            ViewData["name"] = name;
+            ViewData["price"] = price;
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete( int ID)
+        {
+            return new JsonResult(new Product()
+            {
+                id = ID
+            });
+        }
+    }
+}
